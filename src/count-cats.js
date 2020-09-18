efc3d2695011be197edf1a7b5242aad43f14001f
darkspeaker <graggg13@gmail.com>
2020-09-18 08:08:36 +0300

    fix: speed test for task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,5 @@
 module.exports = function countCats(catsImFindYou) {
   let count = 0;
-  catsImFindYou.forEach(i => i.map(item => item === "^^" ? count += 1 : 0))
+  catsImFindYou.forEach(i => catsImFindYou.length > 1 ? i.map(item => item === "^^" ? count += 1 : 0) : i === "^^" ? count += 1 : 0)
   return count
 };
