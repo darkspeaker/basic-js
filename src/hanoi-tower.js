e53a7531c44e86d73f9685362bb1c949f46c162e
darkspeaker <graggg13@gmail.com>
2020-09-27 11:41:04 +0300

    fix: hanoi-tower arg

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,7 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  const turns =  Math.pow(2, diskNumber) - 1
+  const turns =  Math.pow(2, disksNumber) - 1
   const seconds = Math.floor(turns * 3600 / turnsSpeed)
   const obj = {}
   obj.turns = turns
