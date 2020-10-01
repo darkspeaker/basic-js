05ea9fdaa4a4dd0276c02ff614171a9fc21c0df6
darkspeaker <graggg13@gmail.com>
2020-10-01 16:32:02 +0300

    fix: test

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -3,46 +3,38 @@ const CustomError = require("../extensions/custom-error");
 const chainMaker = {
   value: "",
   numberValue: 0,
-  id: 1,
   arr: [],
-  objVal: {},
-  getLength(){
-      return this.numberValue
+  getLength() {
+    return this.numberValue;
   },
   addLink(link = " ") {
-      // this.value = ""
-      // if(typeof a !== "number"){
-      //     throw new Error()
-      // }
-      this.numberValue++
-      this.value += `(${link})~~`;
-      this.arr.push(`(${link})~~`)
-      // this.objVal[`${this.id}`] = [...this.arr];
-      
-      
-      // this.value = this.value.split('+')[0];
-      return this;
+    this.numberValue++;
+    this.value += `( ${link} )~~`;
+    this.arr.push(`( ${link} )~~`);
+    return this;
+  },
+  reverseChain() {
+    this.arr = this.arr.reverse();
+    this.value = this.arr.join("");
+    return this;
   },
   removeLink(position) {
-      position -=  1
-      if(typeof position !== "number"){
-          throw new Error()
-      }
-      // this.value = this.value.replace(/[()]/g, "").split('').slice(position).map(i => "(" + i + ")").join("")
-      // this.value = this.arr.slice(0, position).join("")
-      // console.log(this.arr.slice(position))
-      this.value = this.arr.filter(item => item !== position).join("")
-      return this
-  },
-  reverseChain(){
-      // this.value = this.value.split().reverse().join("")
-      this.value = [...this.arr.reverse()].join("")
-      return this
+    position -= 1;
+    if (position > this.numberValue || position <= 0) {
+      this.arr = [];
+      this.value = "";
+      throw new Error();
+    }
+    this.arr = this.arr.filter((item, index) => index !== position);
+    this.value = this.arr.join("");
+    return this;
   },
   finishChain() {
-      // console.log(typeof this.value)
-      return this.arr.join("").slice(0, -2)   
-  }
+    let temp = this.arr.join("").slice(0, -2);
+    this.value = "";
+    this.arr = [];
+    return temp;
+  },
 };
 
 module.exports = chainMaker;
