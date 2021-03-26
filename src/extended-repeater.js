9f66c5ef538286773df9c31ddc3a420b54514d45
darkspeaker <graggg13@gmail.com>
2021-03-27 01:53:02 +0200

    feat: done extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  let res = ''
+  let helpRes = ''
+  if(options.repeatTimes === undefined && options.additionRepeatTimes === undefined){
+    res = str + options.addition
+  }
+  if(options.separator === undefined){
+    options.separator = '+'
+  }
+  if(options.additionSeparator === undefined){
+    options.additionSeparator = '|'
+  }
+
+  if(options.additionSeparator){
+    for(let i = 0; i < options.additionRepeatTimes; i++){
+      helpRes += options.addition + '' + options.additionSeparator
+    }
+    for(let i = 0; i < options.repeatTimes; i++){
+      res += str + '' + options.separator
+    }
+  }
+  else{
+    for(let i = 0; i < options.repeatTimes; i++){
+      if(options.addition){
+        res += str + '' + options.addition + '' + options.separator
+      }
+      else{
+        res += str + '' + options.separator
+      }
+    }  
+  }
+
+  res = res.split(`${options.separator}`).filter(i => i !== "").join(`${options.separator}`)
+
+  helpRes = helpRes.split(`${options.additionSeparator}`).filter(i => i !== "").join(`${options.additionSeparator}`)
   
+  res = res.split(`${options.separator}`).map(i => i + helpRes).join(`${options.separator}`)
+
+  return res
+};
